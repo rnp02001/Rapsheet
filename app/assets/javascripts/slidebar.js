b542aae46cdfb606f7ce910b59c92e7b5ca7732d
MatthewKrey <kreyenglish@gmail.com>
2015-09-30 16:08:49 -0400

    adding additional Timeline styling

--- app/assets/javascripts/slidebar.js
+++ app/assets/javascripts/slidebar.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
     ticks: [1960, 1970, 1980, 1990, 2000, 2010],
     ticks_positions: [0, 20, 40, 60, 80, 100, 120],
     ticks_labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
-    ticks_snap_bounds: 1
+    ticks_snap_bounds: 30
   })
 });
