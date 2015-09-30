019f0c5ff6a3d6d841f30c221cbca9bd70267a74
Ryan Ponte <ryan.ponte@gmail.com>
2015-09-30 16:09:29 -0400

    bug fix add commas to associations

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,5 +1,5 @@
 class State < ActiveRecord::Base
   has_many :annual_state_crime_rates
-  has_many :years through: :annual_state_crime_rates
-  has_many :crimes through: :annual_state_crime_rates
+  has_many :years, through: :annual_state_crime_rates
+  has_many :crimes, through: :annual_state_crime_rates
 end
