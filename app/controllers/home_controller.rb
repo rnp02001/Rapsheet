5bee276945d21a9a7d47de8ce31e3bc715cf23d1
RyanNovas <ryan@quill.org>
2015-09-30 18:26:28 -0400

    working hardcoded map!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+
   def index
     gon.rate = AnnualStateCrimeRate.limit(10)
   end
+  # 
+  # def self.year(year_id, crime_id)
+  #   raw_data = AnnualStateCrimeRate.where(year_id: year_id, crime_id: crime_id)
+  #   year_data = raw_data.sort("rate DESC")
+  # end
+  #
+  # def self.range(year_data)
+  #   min = year_data.first.rate
+  #   median_index = year_data.count / 2
+  #   median = year_data[median_index].rate
+  #   max = year_data.last.rate
+  # end
+
+
 end
--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,5 +1,5 @@
 class Crime < ActiveRecord::Base
   has_many :annual_state_crime_rates
-  has_many :states through: :annual_state_crime_rates
-  has_many :years through: :annual_state_crime_rates
+  has_many :states, through: :annual_state_crime_rates
+  has_many :years, through: :annual_state_crime_rates
 end
