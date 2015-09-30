61de7015f263ab9bb97439358bb89d3d50b22833
MatthewKrey <kreyenglish@gmail.com>
2015-09-30 18:46:56 -0400

    messing with timeline slider bar

--- app/assets/javascripts/slidebar.js
+++ app/assets/javascripts/slidebar.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
   $("#ex13").slider({
-    ticks: [1960, 1970, 1980, 1990, 2000, 2010],
-    ticks_positions: [0, 20, 40, 60, 80, 100, 120],
+    ticks: [0, 20, 40, 60, 80, 100],
+    ticks_positions: [0, 20, 40, 60, 80, 100],
     ticks_labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
-    ticks_snap_bounds: 30
   })
 });
