cd541ccdfdc3f60f4fe4fe4df4fdbf8e31c3b662
MatthewKrey <kreyenglish@gmail.com>
2015-09-30 16:00:15 -0400

    adding timeline formatting

--- app/assets/javascripts/slidebar.js
+++ app/assets/javascripts/slidebar.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#ex13").slider({
-    ticks: [0, 100, 200, 300, 400],
-    ticks_positions: [0, 30, 60, 70, 90, 100],
-    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
-    ticks_snap_bounds: 30
+    ticks: [1960, 1970, 1980, 1990, 2000, 2010],
+    ticks_positions: [0, 20, 40, 60, 80, 100, 120],
+    ticks_labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
+    ticks_snap_bounds: 1
   })
-}); 
+});
