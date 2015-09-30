3086fcfaa1fd427d2de6a1868bb6179324145b3e
Ryan Ponte <ryan.ponte@gmail.com>
2015-09-30 15:50:34 -0400

    add associations to year state crime and rate

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,5 @@
 class State < ActiveRecord::Base
+  has_many :annual_state_crime_rates
+  has_many :years through: :annual_state_crime_rates
+  has_many :crimes through: :annual_state_crime_rates
 end
