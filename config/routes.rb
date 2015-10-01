30bb4c043d3ccebdd667b11220343e56eee1555f
MatthewKrey <kreyenglish@gmail.com>
2015-10-01 11:43:28 -0400

    adding comments model, route, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  #root 'home#index'
+  root to: 'comments#index'
 
   devise_for :users
 
   resources :annual_state_crime_rates
-    get '/annual_state_crime_rates/rate_range/:id', to: 'annual_state_crime_rates#rate_range'  
+    get '/annual_state_crime_rates/rate_range/:id', to: 'annual_state_crime_rates#rate_range'
   resources :years
 
   resources :crimes
 
   resources :states
+
+  resources :comments, only: [:index, :new, :create]
 
 end
