9d3450ef0d21ab0ce43f522c28568c24c25ca197
RyanNovas <ryan@quill.org>
2015-10-01 15:35:18 -0400

    working crime id form

--- app/controllers/annual_state_crime_rates_controller.rb
+++ app/controllers/annual_state_crime_rates_controller.rb
@@ -3,13 +3,18 @@ class AnnualStateCrimeRatesController < ApplicationController
 
   end
 
+
+
   def range_year_crime #This should be refactored into the database or a rake task.
+    binding.pry
     range = AnnualStateCrimeRate.range_for_crime(params["crime_id"])
     gon.min = range.min
     gon.max = range.max
     gon.year_data = AnnualStateCrimeRate.rates_for_year_and_crime(params["year_id"], params["crime_id"])
-    @crime = 'MURDER'
+    @crimes = Crime.all
     render "home/index"
   end
+
+
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+
+  
+    @crimes = Crime.all
     gon.rate = AnnualStateCrimeRate.limit(10)
   end
 
