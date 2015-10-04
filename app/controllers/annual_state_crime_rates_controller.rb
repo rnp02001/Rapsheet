c3b5b15850763347dff39386c58b323e8469dc20
Ryan Novas <RyanNovas@users.noreply.github.com>
2015-10-04 13:30:36 -0400

    Revert "Restored ajax"

--- app/controllers/annual_state_crime_rates_controller.rb
+++ app/controllers/annual_state_crime_rates_controller.rb
@@ -4,15 +4,15 @@ class AnnualStateCrimeRatesController < ApplicationController
   end
 
 
+
   def range_year_crime #This should be refactored into the database or a rake task.
     range = AnnualStateCrimeRate.range_for_crime(params["id"])
     gon.global.min = range.min
     gon.global.max = range.max
     gon.global.year_data = AnnualStateCrimeRate.rates_for_year_and_crime(params["form_year"], params["id"])
+
     @crimes = Crime.all
-    respond_to do |format|
-      format.js { render 'home/index' }
-    end
+    redirect_to root_path
   end
 
 
