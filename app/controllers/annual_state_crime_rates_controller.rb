007620884dc92038a46078906cab0db66fb21bc0
MatthewKrey <kreyenglish@gmail.com>
2015-10-04 14:34:29 -0400

    resolving loss of map

--- app/controllers/annual_state_crime_rates_controller.rb
+++ app/controllers/annual_state_crime_rates_controller.rb
@@ -5,12 +5,12 @@ class AnnualStateCrimeRatesController < ApplicationController
 
 
 
-  def range_year_crime #This should be refactored into the database or a rake task.
+  def range_year_crime #This should be refactored into the database or a rake task. d
     range = AnnualStateCrimeRate.range_for_crime(params["crime"])
     gon.global.min = range.min
     gon.global.max = range.max
     gon.global.year_data = AnnualStateCrimeRate.rates_for_year_and_crime(params["form_year"], params["crime"])
-
+    binding.pry
     @crimes = Crime.all
     redirect_to root_path
   end
