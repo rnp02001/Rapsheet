06893e73d0062f2f7522f1831be8e92bee51e6ef
RyanNovas <ryan@quill.org>
2015-10-07 18:22:33 -0400

    adds users comments to their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @states = State.all
+    @comments = Comment.all.where(user_id: current_user.id)
   end
 
   def update
