f12a171496f4b8ca8ce85e5817f244356fe7b103
Ryan Ponte <ryan.ponte@gmail.com>
2015-10-07 13:15:24 -0400

    unfinished play button for animating map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,12 +14,9 @@ $(document).on('ready page:load', function(){
  });
 });
 
-
-
-
 var dataManipulators =  function(minimums, maximums, states){
   var series = states;
-  // [['AL', 8], ['AK', 3], ['AZ', 7], ['AR', 8], ['CA', 3], ['CO', 6], ['CT', 7], ['DE', 8], ['DC', 1], ['FL', 6], ['GA', 1], ['HI', 5], ['ID', 1], ['IL', 4], ['IN', 4], ['IA', 5], ['KS', 2], ['KY', 4], ['LA', 6], ['ME', 9], ['MD', 2], ['MA', 7], ['MI', 3], ['MN', 5], ['MS', 4], ['MO', 5], ['MT', 7], ['NE', 2], ['NV', 3], ['NH', 2], ['NJ', 9], ['NM', 7], ['NY', 0], ['NC', 0], ['ND', 6], ['OH', 6], ['OK', 3], ['OR', 1], ['PA', 1], ['RI', 7], ['SC', 1], ['SD', 0], ['TN', 7], ['TX', 9], ['UT', 7], ['VT', 9], ['VA', 2], ['WA', 1], ['WV', 6], ['WI', 2], ['WY', 4], ['US', 9]];
+  // [['AL', 8], ['AK', 3], ['AZ', 7], ['AR', 8], ['CA', 3], ['CO', 6], ['CT', 7], ['DE', 8], ['FL', 6], ['GA', 1], ['HI', 5], ['ID', 1], ['IL', 4], ['IN', 4], ['IA', 5], ['KS', 2], ['KY', 4], ['LA', 6], ['ME', 9], ['MD', 2], ['MA', 7], ['MI', 3], ['MN', 5], ['MS', 4], ['MO', 5], ['MT', 7], ['NE', 2], ['NV', 3], ['NH', 2], ['NJ', 9], ['NM', 7], ['NY', 0], ['NC', 0], ['ND', 6], ['OH', 6], ['OK', 3], ['OR', 1], ['PA', 1], ['RI', 7], ['SC', 1], ['SD', 0], ['TN', 7], ['TX', 9], ['UT', 7], ['VT', 9], ['VA', 2], ['WA', 1], ['WV', 6], ['WI', 2], ['WY', 4]];
 
   var paletteScale = d3.scale.linear()
      .domain([minimums, maximums])
@@ -42,9 +39,10 @@ var dataManipulators =  function(minimums, maximums, states){
       scope: 'usa',
       data: dataset,
       responsive: true,
+      legend: true,
       geographyConfig: {
         highlightFillColor: function(geo) {
-            return geo['fillColor'] || '#F5F5F5';
+            return geo.fillColor || '#F5F5F5';
           },
              popupTemplate: function(geo, data) {
                  return ['<div class="hoverinfo"><strong>',
@@ -67,4 +65,6 @@ var dataManipulators =  function(minimums, maximums, states){
   function outputUpdate(year) {
   	document.querySelector('#year').value = year;
     $("#form_year").val(year);
-  };
+  }
+
+  
