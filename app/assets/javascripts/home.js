25233b0f40845577a681573f423230594e249ee3
RyanNovas <ryan@quill.org>
2015-10-07 17:26:09 -0400

    ajax goes to js.erb

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -64,5 +64,3 @@ var dataManipulators =  function(minimums, maximums, states){
   	document.querySelector('#year').value = year;
     $("#form_year").val(year);
   }
-
-$("#show-comments").on('click',toggle("#hide-comments"));
