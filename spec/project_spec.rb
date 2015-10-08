0cbcb0e41ec532abed9b76b9f4c61163a4bdb9d4
MatthewKrey <kreyenglish@gmail.com>
2015-10-08 15:28:45 -0400

    configuring test environment in spec directory for testing login support and factory girl

--- spec/project_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-
-Rspec.describe User, type: :model do
-  it "is created when it has a username, email and password" do
-    user = User.new
-    expect(project.valid?).to be false
-  end
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,10 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include FactorGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
