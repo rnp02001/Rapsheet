f7586154f98ba545e041dcf981945ce24fa2caf9
RyanNovas <ryan@quill.org>
2015-10-08 10:50:09 -0400

    more refactoring

--- app/models/annual_state_crime_rate.rb
+++ app/models/annual_state_crime_rate.rb
@@ -14,7 +14,7 @@ class AnnualStateCrimeRate < ActiveRecord::Base
   def self.rates_for_year_and_crime(year_id, crime_id)
     year_raw_data = AnnualStateCrimeRate.all.where(year_id: Year.find_by(year: year_id.to_i).id, crime_id: crime_id)
     array = year_raw_data.map {|item| [item.state.abbr, item.rate]}
-    array.sort_by!{|s,r|r}.reverse
+    array.sort_by!{|s,r|r}.reverse!
     array.each_with_index.map {|item, index| item.push(index)}
   end
 
