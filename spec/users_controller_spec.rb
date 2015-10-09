ffab1c1cbbc0139a6cc017cfb66867187f5a8ef1
MatthewKrey <kreyenglish@gmail.com>
2015-10-09 08:02:23 -0400

    adding first user model tests. staying up late did not help

---
+++ spec/users_controller_spec.rb
@@ -0,0 +1,16 @@
+# require 'rails_helper'
+#
+# RSpec.describe UsersController, type: :controller
+#
+#   describe "anonymous user" do
+#     before :each do
+#       # This simulates an anonymous user
+#       login_with nil
+#     end
+#
+#     it "should be redirected to signin" do
+#       get :index
+#       expect( response ).to redirect_to( new_user_registration_path )
+#     end
+#   end
+# end
