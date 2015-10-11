d964033fb168ca9e80bf33c309ea40883bb6a098
MatthewKrey <kreyenglish@gmail.com>
2015-10-10 22:18:55 -0400

    add small comments. improve readability of tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # Factory Test 
+  # Factory Test
   it "should have a factory" do
     expect(FactoryGirl.build(:user)).to be_valid
   end
@@ -24,20 +24,17 @@ RSpec.describe User, type: :model do
     it "is invalid without a password_confirmation" do
       expect(FactoryGirl.build(:user, password_confirmation: nil)).to_not be_valid
     end
-
-    # it "is invalid without a state_id" do
-    #   expect(FactoryGirl.build(:user, state_id: nil)).to_not be_valid
-    # end
   end
-    #Association Tests
-    context "Associations" do
-      it "can have many comments" do
-        user = FactoryGirl.build(:user)
-        comment = FactoryGirl.build(:comment)
-        user.comments << comment
-        user.save # do we need this? Is user not already saved by FactoryGirl.build(:user)?
 
-        expect(user.comments).to include(comment)
-      end
+  # Association Tests
+  context "Associations" do
+    it "can have many comments" do
+      user = FactoryGirl.build(:user)
+      comment = FactoryGirl.build(:comment)
+      user.comments << comment
+      user.save # do we need this? Is user not already saved by FactoryGirl.build(:user)?
+
+      expect(user.comments).to include(comment)
     end
+  end
 end
